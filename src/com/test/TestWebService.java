672a6dc3e0c0eea51b5db74749d4bc8c75bec3d2
晏露明 <yanlm@shxrtech.com>
2019-06-16 22:38:54 +0800

    原始代码

---
+++ src/com/test/TestWebService.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import EDoc2OrganizationWebService.EDoc2OrganizationWebService;
+import EDoc2OrganizationWebService.EDoc2OrganizationWebServiceLocator;
+import EDoc2OrganizationWebService.EDoc2OrganizationWebServiceSoap_PortType;
+
+import javax.xml.rpc.ServiceException;
+
+public class TestWebService {
+    public static void main(String[] args) {
+        EDoc2OrganizationWebServiceLocator eDoc2OrganizationWebServiceLocator = new EDoc2OrganizationWebServiceLocator();
+    }
+}
